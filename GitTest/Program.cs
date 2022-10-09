0d071ed331c687b4c5df55b670979d5a2d9a73c2
acer-swift3 <acer-swift3@LAPTOP-7MQ3G9N7>
2022-10-09 18:12:15 +0800

    加入專案檔案。

---
+++ GitTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = "This is Git Test";
+            Console.WriteLine(A);
+            Console.ReadKey();
+        }
+    }
+}
